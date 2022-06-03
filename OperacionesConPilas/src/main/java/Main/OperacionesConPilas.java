8cd1e91342dcbf9ac036543946c8f6ec5e0a8179
Andres-Mateus <72154227+Andres-Mateus@users.noreply.github.com>
2022-06-03 09:49:16 -0500

    Operaciones con Pilas
    Se agrego ejercicio de evacuación de operaciones en notación posfija.

---
+++ OperacionesConPilas/src/main/java/Main/OperacionesConPilas.java
@@ -0,0 +1,25 @@
+package Main;
+
+import java.util.Scanner;
+//import javax.swing.JOptionPane;
+
+public class OperacionesConPilas {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String entrada = "";
+        
+        do{
+            //entrada = JOptionPane.showInputDialog(null, "Ingrese la expresión a evaluar. \nDigite 'ninguna' para terminar la evaluación.");
+            System.out.println("Ingrese la expresión a evaluar.");
+            entrada = sc.nextLine();
+            
+            if(entrada != ""){
+                
+            }
+            
+            
+        }while(entrada != "");
+    }
+}
