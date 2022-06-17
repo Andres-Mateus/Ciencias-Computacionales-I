77b7ddd27ed016cb279595fc7b5510eac3a68b16
Andres-Mateus <72154227+Andres-Mateus@users.noreply.github.com>
2022-06-17 09:28:33 -0500

    Detalles Menores Cola De Prioridad

--- ClientesColaDePrioridad/src/main/java/Main/Main.java
+++ ClientesColaDePrioridad/src/main/java/Main/Main.java
@@ -14,12 +14,14 @@ public class Main {
         Cliente c3 = new Cliente("Amanda", false, true, 22);
         Cliente c4 = new Cliente("Rojelio", true, false, 30);
         Cliente c5 = new Cliente("Lilia", true, true, 60);
+        Cliente c6 = new Cliente("Lilia", false, false, 60);
         
         clientes.add(c1);
         clientes.add(c2);
         clientes.add(c3);
         clientes.add(c4);
         clientes.add(c5);
+        clientes.add(c6);
         
         Collections.sort(clientes, new CompararCliente());
         
